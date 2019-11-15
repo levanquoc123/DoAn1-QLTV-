5c87df1d9b5a9533920c27da3d3936b579965483
levanquoc123 <17110212@student.hcmute.edu.vn>
2019-11-15 19:33:23 +0700

    r

--- quản lý thư viện/quản lý thư viện/yeucauhotro.cs
+++ quản lý thư viện/quản lý thư viện/yeucauhotro.cs
@@ -85,10 +85,5 @@ namespace quản_lý_thư_viện
             }
             return ma;
         }
-
-        //private void yeucauht_Load_1(object sender, EventArgs e)
-        //{
-        //    Loaddata();
-        //}
     }
 }
